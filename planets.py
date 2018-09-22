f65654c04fea493c81ebcdbf5870343df445cad2
takumiarai <43486387+takumiarai@users.noreply.github.com>
2018-09-21 18:58:40 -0700

    second commit

--- planets.py
+++ planets.py
@@ -1,13 +1,11 @@
 def weight_on_planets():
-    earthwt = raw_input ("Enter your weight: ")
+    earthwt = int(input("Enter your weight: "))
     marswt = earthwt * 0.38
     jupiterwt = earthwt * 2.34
-    print ("What do you weigh on earth? ", earthwt,
-           "\n","\n"
-           "On Mars you weigh ", marswt, " pounds.", 
-           "\n",
-           "On Jupiter you weigh ", jupiterwt, " pounds.")
-   
-   
+    print("What do you weigh on earth? " + str(earthwt) + "\n" +
+          "On Mars you weigh " + str(marswt) + " pounds." + "\n" +
+          "On Jupiter you weigh " + str(jupiterwt) + " pounds.")
+
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
