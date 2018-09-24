9a006978c302e5b24be76a9c13d3ed05aef7645b
takumiarai <43486387+takumiarai@users.noreply.github.com>
2018-09-23 18:30:29 -0700

    third commit
    took out unnecessary quotations

--- planets.py
+++ planets.py
@@ -2,10 +2,8 @@ def weight_on_planets():
     earthwt = int(input("Enter your weight: "))
     marswt = earthwt * 0.38
     jupiterwt = earthwt * 2.34
-    print("What do you weigh on earth? " + str(earthwt) + "\n" +
-          "On Mars you weigh " + str(marswt) + " pounds." + "\n" +
-          "On Jupiter you weigh " + str(jupiterwt) + " pounds.")
-
+    print("What do you weigh on earth? \nOn Mars you weigh "
+          + str(marswt) + " pounds.\nOn Jupiter you weigh " + str(jupiterwt) + " pounds.")
 
 if __name__ == '__main__':
     weight_on_planets()
